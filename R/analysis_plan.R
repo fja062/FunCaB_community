61603be6ae66ffc0f96d03b2507a6216b48d4cb0
audhalbritter <me@audhalbritter.com>
2025-04-15 16:00:01 +0200

    add diversity function

--- R/analysis_plan.R
+++ R/analysis_plan.R
@@ -1,6 +1,13 @@
 # data analysis plan
 analysis_plan <- list(
 
+    # calculate diversity metrics
+    tar_target(
+      name = diversity,
+      command = calc_diversity(cover)
+    ),
+
+
   # make pca
   tar_target(
     name = community_pca,
