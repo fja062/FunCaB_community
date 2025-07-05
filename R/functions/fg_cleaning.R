b44aa18790d62515804302651f4f87d2c6ff41d1
audhalbritter <me@audhalbritter.com>
2025-07-05 09:55:35 +0200

    Refactor fg_cleaning function for improved readability and consistency; adjust spacing, enhance blockID mapping, and streamline data manipulation steps.

--- R/functions/fg_cleaning.R
+++ R/functions/fg_cleaning.R
@@ -1,4 +1,4 @@
-fg_cleaning <- function(community_raw, gridded_climate){
+fg_cleaning <- function(community_raw, gridded_climate) {
   transition_community <- community_raw %>%
     # cleaning blockIDs
     mutate(blockID = case_when(
@@ -7,29 +7,28 @@ fg_cleaning <- function(community_raw, gridded_climate){
       plotID == "Fau3XC" ~ "Fau5",
       plotID == "Gud1XC" ~ "Gud12",
       plotID == "Gud4XC" ~ "Gud15",
+      plotID == "Ves4C" ~ "Ves4",
       TRUE ~ blockID
     )) |>
-    
     # remove single occurence species
-    filter(! species %in% c("Dan.dec", "Ver.ver")) |> 
-    
+    filter(!species %in% c("Dan.dec", "Ver.ver")) |>
     # fix missing total_graminoids and total_forbs
-    mutate(total_graminoids = case_when(
-      is.na(total_graminoids) & plotID == "Fau1C" & species == "Hol.lan" ~ 70,
-      is.na(total_graminoids) & plotID == "Fau2C" & species == "Hol.lan" ~ 60,
-      is.na(total_graminoids) & plotID == "Ram5C" & species == "Fes.viv" ~ 50,
-      is.na(total_graminoids) & plotID == "Vik5C" & species == "Hol.lan" ~ 45,
-      TRUE ~ total_graminoids
-    ),
-    total_forbs = case_when(
-      is.na(total_forbs) & plotID == "Fau1C" & species == "Hol.lan" ~ 70,
-      is.na(total_forbs) & plotID == "Fau2C" & species == "Hol.lan" ~ 25,
-      is.na(total_forbs) & plotID == "Ram5C" & species == "Fes.viv" ~ 40,
-      is.na(total_forbs) & plotID == "Vik5C" & species == "Hol.lan" ~ 60,
-      TRUE ~ total_forbs
-    )
-  ) |> 
-    
+    mutate(
+      total_graminoids = case_when(
+        is.na(total_graminoids) & plotID == "Fau1C" & species == "Hol.lan" ~ 70,
+        is.na(total_graminoids) & plotID == "Fau2C" & species == "Hol.lan" ~ 60,
+        is.na(total_graminoids) & plotID == "Ram5C" & species == "Fes.viv" ~ 50,
+        is.na(total_graminoids) & plotID == "Vik5C" & species == "Hol.lan" ~ 45,
+        TRUE ~ total_graminoids
+      ),
+      total_forbs = case_when(
+        is.na(total_forbs) & plotID == "Fau1C" & species == "Hol.lan" ~ 70,
+        is.na(total_forbs) & plotID == "Fau2C" & species == "Hol.lan" ~ 25,
+        is.na(total_forbs) & plotID == "Ram5C" & species == "Fes.viv" ~ 40,
+        is.na(total_forbs) & plotID == "Vik5C" & species == "Hol.lan" ~ 60,
+        TRUE ~ total_forbs
+      )
+    ) |>
     # imputing 0s in moss height where moss cover is low
     mutate(moss_height = case_when(
       (is.na(moss_height) & total_bryophytes <= 5) ~ 0,
@@ -40,10 +39,9 @@ fg_cleaning <- function(community_raw, gridded_climate){
     mutate(sum_cover = sum(cover)) |>
     # keep only the TTC controls in Alrust
     filter(!(plotID == "Alr3C" & is.na(turfID))) %>%
-    
+    ungroup() %>%
     # fix plotIDs
     funcabization(., convert_to = "Funder") %>%
-    
     # make data fancy
     make_fancy_data(., gridded_climate, fix_treatment = TRUE) %>%
     select(-sumcover) |>
@@ -52,13 +50,15 @@ fg_cleaning <- function(community_raw, gridded_climate){
   impute_ctrls <- transition_community |>
     left_join(
       transition_community |>
-        select(year:fg_removed, fg_remaining, vegetation_height, moss_height, total_graminoids, total_forbs, total_bryophytes) |>
+        select(year:fg_removed, fg_remaining, vegetation_height, moss_height,
+        total_graminoids, total_forbs, total_bryophytes) |>
         filter(fg_removed == "C") |>
         distinct() |>
         group_by(plotID) |>
         mutate(moss_height2 = case_when(
           (is.na(moss_height)) ~ mean(moss_height, na.rm = TRUE),
-          TRUE ~ moss_height))
+          TRUE ~ moss_height
+        ))
     ) |>
     mutate(moss_height = if_else((is.na(moss_height) & fg_removed == "C"), moss_height2, moss_height)) |>
     select(-moss_height2) |>
@@ -66,19 +66,21 @@ fg_cleaning <- function(community_raw, gridded_climate){
 
   impute_trts <- impute_ctrls |>
     left_join(impute_ctrls |>
-                select(year:fg_removed, fg_remaining, vegetation_height, moss_height, total_graminoids, total_forbs, total_bryophytes) |>
-                tidylog::distinct() |>
-                #filter(fg_removed %in% c("C", "XC")) |>
-                group_by(siteID, year) |>
-                mutate(site_moss_height = mean(moss_height, na.rm = TRUE),
-                       moss_height2 = if_else(is.na(moss_height), site_moss_height, moss_height))
+      select(year:fg_removed, fg_remaining, vegetation_height, moss_height,
+      total_graminoids, total_forbs, total_bryophytes) |>
+      tidylog::distinct() |>
+      # filter(fg_removed %in% c("C", "XC")) |>
+      group_by(siteID, year) |>
+      mutate(
+        site_moss_height = mean(moss_height, na.rm = TRUE),
+        moss_height2 = if_else(is.na(moss_height), site_moss_height, moss_height)
+      )) |>
+    mutate(
+      moss_height = if_else((is.na(moss_height)), moss_height2, moss_height),
+      imputed_height = if_else(moss_height == site_moss_height, TRUE, FALSE)
     ) |>
-    mutate(moss_height = if_else((is.na(moss_height)), moss_height2, moss_height),
-           imputed_height = if_else(moss_height == site_moss_height, TRUE, FALSE)) |>
     select(-moss_height2, -site_moss_height) |>
     ungroup()
 
   impute_trts
 }
-
-
